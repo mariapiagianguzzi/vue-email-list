74d883b8a9462be55709e3c3c6d4744774018b28
Mariapia Gianguzzi <piagianguzzi@gmail.com>
2021-01-08 15:21:31 +0100

    vue-email-list finito

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 /* Istruzioni:
 Genera 10 mail tramite api e stampale in una lista
 Usate Vue e Axios. */
+
+let app = new Vue({
+  el: "#app",
+  data: {
+    emails: [],
+  },
+  mounted() {
+    for (var i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((response) => {
+          console.log(response.data.response);
+          let email = response.data.response;
+          this.emails.push(email);
+        });
+    }
+  },
+});
